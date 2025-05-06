09ebc6a6614de72e37a98209803f2595d6b43e06
claxiux <hosein.mirzai@tum.de>
2025-05-06 11:02:13 +0200

    Add EntryA.js and EntryB.js with HandMadeRug definitions
    This commit introduces two new files, `EntryA.js` and `EntryB.js`, which define lists of HandMadeRug objects. These files import the `HandMadeRug` class and use it to create instances with predefined attributes.

---
+++ src/containers/EntryA.js
@@ -0,0 +1,8 @@
+import { HandMadeRug } from '@/containers/CarpetMeta.container';
+
+
+const handMadeRugsListA = [
+  new HandMadeRug('productID', 'internal comments', '', 1, 123, null, 0, {0: 1}, [], [], [],[], 1, 1, [], [0], [], null, ''),
+]
+
+// new HandMadeRug('productID', 'internal comments', '', 1, 123, null, 0, {0: 1}, [], [], [],[], 1, 1, [], [0], [], null, ''),
