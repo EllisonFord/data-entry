f773dfc450a24caabbb536e17959964eb4a00ca7
Sam <moka7452@gmail.com>
2025-05-07 14:50:00 +0200

    Prepping class

--- src/containers/EntryA.js
+++ src/containers/EntryA.js
@@ -2,7 +2,7 @@ import { HandMadeRug } from '@/containers/CarpetMeta.container';
 
 
 const handMadeRugsListA = [
-  new HandMadeRug('productID', 'internal comments', '', 1, 123, null, 0, {0: 1}, [], [], [],[], 1, 1, [], [0], [], null, ''),
+  new HandMadeRug('productID', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [],[], 1, 1, [], [0], [], null, ''),
 ]
 
-// new HandMadeRug('productID', 'internal comments', '', 1, 123, null, 0, {0: 1}, [], [], [],[], 1, 1, [], [0], [], null, ''),
+// new HandMadeRug('productID', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [],[], 1, 1, [], [0], [], null, ''),
