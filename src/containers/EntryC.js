13c9c2061a47b898e54eeb59aa31b458f0b91394
sam <moka7452@gmail.com>
2025-05-14 13:35:45 +0200

    Update `region` field to '3' for multiple HandMadeRug entries
    The `region` field was updated for several `HandMadeRug` instances in `CarpetInstances.container.js`, changing its value to '3'. This adjustment enhances data consistency across the specified entries.

--- src/containers/EntryC.js
+++ src/containers/EntryC.js
@@ -2,7 +2,17 @@ import { HandMadeRug } from '@/containers/CarpetMeta.container';
 
 
 const handMadeRugsListC = [
-  new HandMadeRug('productID', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [],[], 1, 1, [], [0], [], null, ''),
+  new HandMadeRug('475', 'blue one that was dyed recently', 'external comments', 1, 2377, null, 0, {0: 1}, [], [], [0],[], 166, 95, [], [0], [], null, '3'),
+  new HandMadeRug('4368', '', 'external comments', 1, 1122, null, 0, {0: 1}, [], [], [0],[], 170, 110, [], [0], [], null, '3'),
 ]
 
-// new HandMadeRug('productID', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [],[], 1, 1, [], [0], [], null, ''),
+ new HandMadeRug('5333', 'internal comments', 'external comments', 1, 1186, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
+
+ new HandMadeRug('id', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
+ new HandMadeRug('id', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
+ new HandMadeRug('id', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
+ new HandMadeRug('id', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
+ new HandMadeRug('id', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
+ new HandMadeRug('id', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
+ new HandMadeRug('id', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
+ new HandMadeRug('id', 'internal comments', 'external comments', 1, 123, null, 0, {0: 1}, [], [], [0],[], 162, 122, [], [0], [], null, '3'),
